bfd034ab7423076f2c5748a8dc0ee36c71d20dab
ste claudino daffara <ste.daff@gmail.com>
2014-02-03 20:29:25 -0800

    first commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AnkrVideo::Application.config.session_store :cookie_store, :key => '_ankr_video_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# YoutubeItRailsAppExample::Application.config.session_store :active_record_store
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+AnkrVideo::Application.routes.draw do
+  resources :videos do
+    member do
+      post :add_comment
+    end     
+    new do
+       post :upload
+       get  :save_video
+     end
+  end
+
+  match "videos/:id/add_comment", :to => "videos#add_comment"
+
+  root :to => "videos#index"
+end
